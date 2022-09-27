e93b3357077aa85d20ada318a32357dff878a0d2
Julian Lancheros <47438320+jlancherosnn@users.noreply.github.com>
2022-09-27 12:51:57 -0500

    .

--- src/app.py
+++ src/app.py
@@ -1,4 +1,10 @@
-from flask import app
+from flask import app,Flask, render_template, request, redirect, url_for, flash
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
