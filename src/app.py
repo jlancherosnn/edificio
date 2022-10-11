640e15e92f49953b5dcdb444bfb3b4c5d566d999
YenyVivianaMolano <79315982+YenyVivianaMolano@users.noreply.github.com>
2022-10-10 20:36:58 -0500

    Hola

--- src/app.py
+++ src/app.py
@@ -12,6 +12,7 @@ def index():
 @app.route('/registro')
 def registro():
     return render_template('registro.html')
+    
 @app.route('/index')
 def index1():
     return render_template('index.html')
